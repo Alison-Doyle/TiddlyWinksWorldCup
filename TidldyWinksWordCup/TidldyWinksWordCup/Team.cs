164efdbf914757228a9447248d61086fd1eaf984
Blackthrn <blackthorngirl26@hotmail.com>
2023-11-30 12:37:58 +0000

    :tada: Initial commit
    - UI created
    - Added Player and Team classes

---
+++ TidldyWinksWordCup/TidldyWinksWordCup/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TidldyWinksWordCup
+{
+    internal class Team
+    {
+        // Attributes
+        public string Name { get; set; }
+        public List<Player> Players { get; set; }
+
+        // Constructors
+
+        // Methods
+    }
+}
